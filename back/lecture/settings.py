f84cefbb59b65a6fc3df012f015883bd69030090
bugounet <sullivan.matas@gmail.com>
2020-01-06 10:42:00 +0100

    Update settings to make the deploy script work

--- back/lecture/settings.py
+++ back/lecture/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import * from .secret
+from .secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf.global_settings import MEDIA_ROOT
 
